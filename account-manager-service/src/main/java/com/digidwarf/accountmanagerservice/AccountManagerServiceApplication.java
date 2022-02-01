7cf107b554347ea6ed7f8cb289ee27e73df8fa5a
musheghgh <musheghgh@energizeglobal.com>
2022-02-01 17:09:17 +0400

    skeleton 1st

---
+++ account-manager-service/src/main/java/com/digidwarf/accountmanagerservice/AccountManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.digidwarf.accountmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountManagerServiceApplication.class, args);
+    }
+
+}
